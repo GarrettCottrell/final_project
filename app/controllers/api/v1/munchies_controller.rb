633f16ba9611f19e2313d19353fd5161f4362491
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2021-01-19 09:47:02 -0700

    openweathermap api endpoint working in controller. All three endpoints functioning

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -14,13 +14,20 @@ module Api
         conn = Faraday.new('https://api.yelp.com') do |f|
           f.headers['Authorization'] = ENV['YELP_API_KEY']
         end
-  
+
         response = conn.get('/v3/businesses/search?') do |f|
           f.params[:location] = params[:end]
           f.params[:categories] = params[:food]
         end
         json = JSON.parse(response.body, symbolize_names: true)
-        
+
+        conn = Faraday.new(
+          url: 'https://api.openweathermap.org/data/2.5/weather',
+          params: {appid: ENV['APP_ID']}
+        )
+        response = conn.get("?q=#{params[:end]}&units=imperial")
+        json = JSON.parse(response.body, symbolize_names: true)
+        require 'pry'; binding.pry
       end
     end
   end
